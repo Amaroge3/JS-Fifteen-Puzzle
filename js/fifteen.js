d69350e5bb749755c10c98eab7ea3a91347de82a
Andi Maroge <Amaroge3@gmail.com>
2016-05-24 11:59:40 -0700

    added shuffle feature to the shuffle button and updated comments

--- js/fifteen.js
+++ js/fifteen.js
@@ -11,8 +11,9 @@ window.onload = function(){
 	var count = 1;
 	var xBgPos = 0;
 	var yBgPos = 0; 
-		
-// 	document.body.style.position = "relative"; 
+	
+	var shuffleButton = document.getElementById("shufflebutton");						//shuffle button
+
 
 	/* puzzle area dimensions and margin declarations */
 	puzzleArea.style.position = "relative"; 
@@ -25,18 +26,18 @@ window.onload = function(){
 		for (var j = 0; j < 4; j++){
 			
 			newDiv = document.createElement("div");
-						
-			puzzleArea.appendChild(newDiv);		//add new div element to the DOM 
-			newDiv.style.height = "98px";	//set up height of new div element
-			newDiv.style.width= "98px";		//set up width of new div element
-			newDiv.style.border = "1px solid black";	//set up border of .5 pixels solid black
-			newDiv.id = j + "_" + i;	//declare id for the new div
-			newDiv.style.margin = "0";	//declare the margin for new div element
+							
+			puzzleArea.appendChild(newDiv);											//add new div element to the DOM 
+			newDiv.style.height = "98px";											//set up height of new div element
+			newDiv.style.width= "98px";												//set up width of new div element
+			newDiv.style.border = "1px solid black";								//set up border of .5 pixels solid black
+			newDiv.id = j + "_" + i;												//declare id for the new div
+			newDiv.style.margin = "0";												//declare the margin for new div element
 			//add image
-			newDiv.style.backgroundImage = 'url(img/mario_pict.png)'; 	//add the background image of puzzle on each div element
+			newDiv.style.backgroundImage = 'url(img/mario_pict.png)'; 				//add the background image of puzzle on each div element
 			newDiv.style.backgroundPosition = xBgPos + "px" + " " + yBgPos + "px";	//show the specified background position of x and y for each new div
-			newDiv.style.display = "inline-block";	//set puzzles to inline block
-			newDiv.style.verticalAlign = "top";		//remove space between div elements
+			newDiv.style.display = "inline-block";									//set puzzles to inline block
+			newDiv.style.verticalAlign = "top";										//remove space between div elements
 
 			xBgPos -= 100;	//x pixels decrease for next background position iteration
 
@@ -62,17 +63,18 @@ window.onload = function(){
 	children = puzzleArea.children;	//puzzle pieces nodes list
 	
 	/* edit empty div element */  
-	children[15].id = "emp";	//set empty div to empty
-	children[15].style.backgroundColor = "white";	//set background color to white on empty div
-	children[15].style.backgroundImage = "none";	//set background image to none on white div
+	children[15].id = "emp";														//set empty div to empty
+	children[15].style.backgroundColor = "white";									//set background color to white on empty div
+	children[15].style.backgroundImage = "none";									//set background image to none on white div
 	
 	
 	
-	hover();
-var $element = $(children[15]).next().next();
-console.log($element);
-// var shuffleButton = document.getElementById("shufflebutton");
-// shuffleButton.onclick = shuffle;
+	hover();																		//call the hover function
+	
+	shuffleButton.onclick = shuffle;												//when shuffle button clicks, call shuffle function
+
+
+
 };
 
 /*
@@ -108,71 +110,65 @@ function findNeighbor(element){
 var up = $(element).prevAll().eq(3) || null,	right = $(element).next() || null,
 	down = $(element).nextAll().eq(3) || null,	left = $(element).prev() || null,			
 	upRight = $(up).next() || null,				downRight = down.next() || null,
-	downLeft = $(down).prev()|| null, 			upLeft = $(up).prev() || null;
+	downLeft = $(down).prev() || null, 			upLeft = $(up).prev() || null;
 
 	//if left corner is selected
 	switch (element) {
 	case children[0]:
  			
-			if ( isEmptyElement(right) ){	//if right element empty
-			swapRight(element, right);   	//swap right
-			}
-			else if ( isEmptyElement(down) ){	//if down element empty
-			swapDown(element, down, downRight); 
-			}
+		if ( isEmptyElement(right) ){						//if right element empty
+			swapRight(element, right);   					//swap element right
+		}
+		else if ( isEmptyElement(down) ){					//if down element empty
+			swapDown(element, down, "before", downRight);	//swap element down
+		}
 	break;
 
 	//if right corner is selected
 	case children[3]:
 		
-		if ( isEmptyElement(left) ){
-			swapLeft(element, left);   //swap right
-			}
-			else if ( isEmptyElement(down) ){
-				$(element).before(down);
-				$(downLeft).after(element);
-			}
+		if ( isEmptyElement(left) ){						//if left element empty			
+		swapLeft(element, left);							//swap element left 
+		}
+		else if ( isEmptyElement(down) ){					//if down element empty
+			swapDown(element, down, "before", downRight);	//swap element down
+		}
 	break;
 
 	//if left bottom corner is selected
 	case children[12]:
-		
-			if ( isEmptyElement(right) ){	//if right element empty
-			swapRight(element, right);		//swap right
-			}
-			else if ( isEmptyElement(up) ){	
-			swapUp(element, up, upRight); 
-			}	
+	
+		if ( isEmptyElement(right) ){						//if right element empty
+			swapRight(element, right);						//swap element right
+		}
+		else if ( isEmptyElement(up) ){						//if up element empty
+			swapUp(element, up, "before", upRight); 		//swap element up
+		}	
 	break;
 
 	//if right bottom corner is selected when puzzle piece is not empty
 	case children[15]:
 	
-			if ( isEmptyElement(left) ){	//if left element empty
-				swapLeft(element, left);   		//swap left
-			}
-			else if ( isEmptyElement(up) ){	//if up element empty 
-
-				$(element).before(up);
-
-					//swap element up
-				$(upLeft).after(element);	
-			}	
+		if ( isEmptyElement(left) ){						//if left element empty
+			swapLeft(element, left);   						//swap element left
+		}
+		else if ( isEmptyElement(up) ){						//if up element empty 
+			swapUp(element, up, "after", upLeft);			//swap element up
+		}	
 	break;
 	
 	//if middle top elements are selected
 	case children[1]:
 	case children[2]:
 
-		if ( isEmptyElement(left) ){
-			swapLeft(element, left);
-			}
-			else if ( isEmptyElement(right) ){
-				swapRight(element,right);
-			}
-			else if ( isEmptyElement(down) ){
-				$(element).before(down);			//swap element down  
-				$(downLeft).after(element);						
+		if ( isEmptyElement(left) ){						//if left element empty
+			swapLeft(element, left);						//swap element left
+		}
+		else if ( isEmptyElement(right) ){					//if right element empty
+			swapRight(element,right);						//swap element down
+		}
+		else if ( isEmptyElement(down) ){					//if down element empty	
+			swapDown(element, down, "before", downRight);	//swap element down			
 			}
 		break;
 
@@ -180,16 +176,15 @@ var up = $(element).prevAll().eq(3) || null,	right = $(element).next() || null,
 	case children[7]:
 	case children[11]:
 		
-		if ( isEmptyElement(up) ){
-			$(element).before(up);
-			$(upLeft).after(element);
+		if ( isEmptyElement(up) ){							//if top element empty
+			swapUp(element, up, "after", upLeft);			//swap element up
+
 		}
-		else if ( isEmptyElement(left) ){
-			swapLeft( element, left );
+		else if ( isEmptyElement(left) ){					//if left element empty
+			swapLeft( element, left );						//swap element left
 		}
-		else if ( isEmptyElement(down) ){
-			$(element).before(down);
-			$(downLeft).after(element);	
+		else if ( isEmptyElement(down) ){					//if down element empty
+			swapDown(element, down, "after", downLeft); 	//swap element down
 		}
 	break;
 
@@ -197,16 +192,14 @@ var up = $(element).prevAll().eq(3) || null,	right = $(element).next() || null,
 	case children[4]:
 	case children[8]: 
 	
-		if ( isEmptyElement(up) ){ 
-			$(element).before(up);
-			$(upRight).before(element);		
+		if ( isEmptyElement(up) ){ 							//if top element empty
+			swapUp(element, up, "before", upRight);			//swap element up
 		}
-		else if ( isEmptyElement(right) ){
-			swapRight(element,right);
+		else if ( isEmptyElement(right) ){					//if right element empty
+			swapRight(element,right);						//swap element right
 		}
-		else if ( isEmptyElement(down) ){
-			$(element).before(down); //swap up element to current position  
-			$(downLeft).after(element);			
+		else if ( isEmptyElement(down) ){					//if down element empty
+			swapDown(element, down, "before", downRight);   //swap element down	
 		}
 	
 	break;
@@ -215,36 +208,32 @@ var up = $(element).prevAll().eq(3) || null,	right = $(element).next() || null,
 	case children[14]:
 	case children[13]:
 			
-		if ( isEmptyElement(left) ){		//if left is empty
-			swapLeft(element, left);  		//swap element left
+		if ( isEmptyElement(left) ){						//if left is empty
+			swapLeft(element, left);  						//swap element left
 		}
-		else if ( isEmptyElement(up) ){		//if top element is empty
-			$(element).before(up);
-			$(upRight).before(element);		//swap up element with selected
+		else if ( isEmptyElement(up) ){						//if top element is empty
+			swapUp(element, up, "before", upRight);			//swap element up
 		}
-		else if ( isEmptyElement(right) ){	//if right element is empty
-			swapRight(element, right);			//swap right
+		else if ( isEmptyElement(right) ){					//if right element is empty
+			swapRight(element, right);						//swap right
 		}			
 	break;
 	
 	//all other elements (middle)    
 	default: 
-		//local variables for possible positions to move to
-			
-			
-		if ( isEmptyElement(up) ){			//if up element empty 
-			$(element).before(up);			//swap up element 
-			$(upRight).before(element);
+		
+		if ( isEmptyElement(up) ){							//if up element empty 
+			swapUp(element, up, "before", upRight);
 		}
-		else if ( isEmptyElement(right) ){	//if right element empty
-			swapRight(element, right);		//swap right element
+		else if ( isEmptyElement(right) ){					//if right element empty
+			swapRight(element, right);						//swap right element
 		}
-		else if ( isEmptyElement(left) ){	//if left element empty
-			swapLeft(element, left);		//swap left element
+		else if ( isEmptyElement(left) ){					//if left element empty
+			swapLeft(element, left);						//swap left element
 		}
-		else if ( isEmptyElement(down) ){	//if down element empty 
-			$(element).before(down);		//swap down element
-			$(downRight).before(element);	
+		else if ( isEmptyElement(down) ){					//if down element empty 
+			swapDown(element, down, "before", downRight);   //swap element down	
+	
 		}			
 	
 	}
@@ -255,81 +244,227 @@ var up = $(element).prevAll().eq(3) || null,	right = $(element).next() || null,
 /* helper function to determine if neighboring element has an id of "emp" */
 function isEmptyElement(element){
 			
-			if ($(element).attr("id") === "emp"){
-				return true;
-			}
-			else {
-				return false;
-			}
+	if ($(element).attr("id") === "emp"){
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
-function shuffle(element){
-children = puzzleArea.children;
-
-	//if left corner is selected
-	switch (element) {
-	case children[0]:
-		
-	break;
-
-	case children[3]:
+/* shuffle function that is called when a click on the shuffle button occurs */
+function shuffle(){
+	var emptyElement = document.getElementById('emp');
 	
-	break;
+	shufflePuzzles(emptyElement);
+}
 
-	case children[12]:
-		
-	break;
+/* The shuffle function that shuffles all the puzzles. 
+* @param element - The empty element that is passed in by the shuffle function with no arguments.  
+*/
+function shufflePuzzles(element){
+	children = puzzleArea.children;
 
-	case children[15]:
-			
-	break;
+		var randNum = 0;
+		var i = 0;
 	
-	case children[1]:
-	case children[2]:
+	//move puzzle pieces 500 times to empty square
+	while ( i < 500 ){
 		
-	break;
-
-	case children[7]:
-	case children[11]:
-		
-	break;
-
-	case children[4]:
-	case children[8]: 
+	var up = $(element).prevAll().eq(3) || null,	right = $(element).next() || null,
+		down = $(element).nextAll().eq(3) || null,	left = $(element).prev() || null,			
+		upRight = $(up).next() || null,				downRight = down.next() || null,
+		downLeft = $(down).prev() || null, 			upLeft = $(up).prev() || null;
 	
-	break;
-
-	case children[14]:
-	case children[13]:		
+		switch (element) {
 			
+			/* upper left corner puzzle */
+			case children[0]:
+				
+				randNum = Math.floor(Math.random() * 2 + 1);		//generate random number from 1 to 2
+				if( randNum == 1 ){									//if random number == 1
+					swapRight(element, right);						//swap empty element right
+				}
+				else {
+					swapDown(element, down, "before", downRight);	//swap empty element down   
+				}
+			break;
+			
+			/* upper right corner puzzle */
+			case children[3]:
+				
+				randNum = Math.floor(Math.random() * 2 + 1);		//generate random number from 1 to 2
+				if( randNum == 1 ){									//if random number == 1
+					swapLeft(element, left);						//swap empty puzzle left
+				}
+				else {
+					swapDown(element, down, "after", downLeft); 	//swap empty element down   
+				}
+			break;
+			
+			/* left lower corner puzzle */
+			case children[12]:
+				
+				randNum = Math.floor(Math.random() * 2 + 1);		//generate random number from 1 to 2
+				if( randNum == 1 ){									//if random number == 1
+					swapRight(element, right);						//swap empty element right
+				}
+				else {
+					swapUp(element, up, "before", upRight);			//swap empty element up
+				}
+			break;
+			
+			/* right lower corner puzzle */
+			case children[15]:
+				
+				randNum = Math.floor(Math.random() * 2 + 1);		//generate random number from 1 to 2
+				if ( randNum == 1 ){								//if random number == 1
+					swapLeft(element,left);							//swap empty puzzle left
+				}
+				else {
+					swapUp(element, up, "after", upLeft); 			//swap empty element up
+				}
+			break;
+			
+			/* upper middle puzzles */
+			case children[1]:
+			case children[2]:
+				
+				randNum = Math.floor(Math.random() * 3 + 1);		//generate random number from 1 to 3
+				if ( randNum == 1 ){								//if random number == 1
+					swapLeft(element, left);						//swap empty puzzle left
+				}	
+				else if ( randNum == 2 ){							//if random number == 2 
+					swapDown(element, down, "before", downRight); 	//swap empty element down
+				}
+				else {												//if random number == 3 
+					swapRight(element, right);						//swap empty element right
+				}
+			break;
+			
+			/* right middle puzzles */
+			case children[7]:										
+			case children[11]:
+				
+				randNum = Math.floor(Math.random() * 3 + 1);		//generate random number from 1 to 3
+				if ( randNum == 1 ){								//if random number == 1
+					swapUp(element, up, "after", upLeft);			//swap empty element up
+					}
+				else if ( randNum == 2 ){							//if random number == 2
+					swapDown(element, down, "after", downLeft);		//swap empty element down
+				}
+				else{												//if random number == 3
+					swapLeft(element, left);						//swap empty element left
+				}
+			break;
+			
+		/* left middle puzzles */
+		case children[4]:
+		case children[8]: 
+			randNum = Math.floor(Math.random() * 3 + 1);			//generate random number from 1 to 3
+			if ( randNum == 1 ){									//if random number == 1
+				swapUp(element, up, "before", upRight);				//swap empty element up
+			}
+			else if ( randNum == 2 ){								//if random number == 2
+				swapRight(element, right);							//swap empty element right
+			}
+			else {													//if random number == 3
+				swapDown(element, down, "before", downRight);		//swap empty element down    
+			}
+		break;
 		
-	break;
+		/* bottom middle puzzles */
+		case children[14]:
+		case children[13]:		
+			randNum = Math.floor(Math.random() * 3 + 1);			//generate random number from 1 to 3
+			if ( randNum == 1 ){									//if random number == 1
+				swapUp(element, up, "before", upRight);				//swap empty element up
+			}
+			else if ( randNum == 2 ){								//if random number == 2
+				swapRight(element, right);							//swap empty element right
+			}
+			else {													//if random number == 3
+				swapLeft(element, left);							//swap empty element left
+			}		
+		
+		break;
 	
-	default: 
-			
-			
-		
+		/* middle puzzles */
+		default: 
+			randNum = Math.floor(Math.random() * 3 + 1);			//generate random number from 1 to 3
+			if ( randNum == 1 ){									//if random number == 1
+				swapUp(element, up, "before", upRight);				//swap empty element up
+			}
+			else if ( randNum == 2 ){								//if random number == 2
+				swapRight(element, right);							//swap empty element right
+			}
+			else {													//if random number == 3
+				swapDown(element, down, "before", downRight);		//swap empty element down
+			}	
+		}
+	
+	element = document.getElementById('emp');					//get the empty puzzle piece on each iteration
+	children = puzzleArea.children;								//update elements on each iteration
+
+	i++;
 	
 	}
 
 }
 
-function swapUp(currentElement, upElement, currentElementTo){
-	
-	$(currentElement).before(upElement);			//swap up element to current position  
-	$(currentElementTo).before(currentElement);		//swap current element up
+
+/*
+* Helper function to swap elements up in the DOM.
+* @param currentElement - The current element to be passed in to swap
+* @param beforeOrAfter - A string that equals either "before" or "after" that is passed in to indicate which
+*						 direction the currentElement should be when manipulating the DOM.
+* @param currentElementTo - The element to insert before or after to in the DOM.   
+*/
+function swapUp(currentElement, upElement, beforeOrAfter, currentElementTo){
+	/* swap current element before the currentElementTo */
+	if (beforeOrAfter === "before"){
+		$(currentElement).before(upElement);					//swap up element to current position  
+		$(currentElementTo).before(currentElement);				//swap current element up
+	}
+	/* swap current element after the currentElementTo */
+	else if (beforeOrAfter === "after"){
+		$(currentElement).before(upElement);					//swap up element to current position  
+		$(currentElementTo).after(currentElement);				//swap current element up
+	}
 }
 
+/*
+* Helper function to swap element to the right in the DOM.
+* @param currentElement - The element that is to be shifted to the right
+*/
 function swapRight(currentElement, toPos){
-	$(currentElement).before(toPos);	//swap right
+	$(currentElement).before(toPos);							//swap currentElement right
 }
 
-function swapDown(currentElement, downElement, currentElementTo){
-	$(currentElement).before(downElement);			//swap up element to current position  
-	$(currentElementTo).before(currentElement);		//swap current element up
+/*
+* Helper function to swap elements down in the DOM.
+* @param currentElement - The current element to be passed in to swap
+* @param beforeOrAfter - A string that equals either "before" or "after" that is passed in to indicate which
+*						 direction the currentElement should be when manipulating the DOM.
+* @param currentElementTo - The element to insert before or after to in the DOM.   
+*/
+function swapDown(currentElement, downElement, beforeOrAfter, currentElementTo){
+	if (beforeOrAfter === "before"){
+		$(currentElement).before(downElement);					//swap down element to current position  
+		$(currentElementTo).before(currentElement);				//swap current element down
+	}
+	else if (beforeOrAfter === "after"){
+		$(currentElement).before(downElement);					//swap down element to current position 
+		$(currentElementTo).after(currentElement);				//swap current element down  
+	}
 }
+
+/*
+* Helper function to swap element to the left in the DOM.
+* @param currentElement - The element that is to be shifted to the left
+*/
 function swapLeft(currentElement, toPos){			
-	$(toPos).before(currentElement);			//swap element to the left of current element  
+	$(toPos).before(currentElement);							//swap currentElement left
 }
 
 
